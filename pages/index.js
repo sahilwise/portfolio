e8f5ebf974c702847ae4df045ac3e8ff9eb7cccf
sahilnetic <jsthil99@gmail.com>
2023-02-09 00:18:00 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -51,14 +51,14 @@ const Home = ()=>{
                             </p>
 
                             <section className="space-y-4">
-
+    
                             <article>
                                 <header>
                                     <h2 className="text-lg text-neutral-800 flex">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 -translate-x-1 translate-y-3">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
                                     </svg>
-                                        fork on github
+                                      <Link href={"https://github.com/salnetxyz/portfolio/fork"}>fork on github</Link>
                                     </h2>
                                     <p className="text-xs text-neutral-600">github.com/salnetxyz</p>
                                 </header>
