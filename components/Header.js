0cd79c70bb4bd3a2a98bb8cbc30839711c4b78e0
sahilnetic <jsthil99@gmail.com>
2023-02-09 00:51:11 +0530

    added meta og and twitter card

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () =>{
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content="Sal Net" />
             <meta property="og:type" content="website" />
-            <meta propert="og:image" content="/download.jpeg" />
+            <meta propert="og:image" content="https://www.salnet.xyz/download.jpeg" />
             <meta property="og:url" content="https://salnet.xyz" />
             <meta property="og:description" content="Hi This is Sal, I am a Fullstack Developer." />
             <meta property="og:locale" content="en_US" />
@@ -19,7 +19,7 @@ const Header = () =>{
             <meta property="og:url" content="http://salnet.xyz/" />
             <meta property="og:title" content="A Twitter for My Sister" />
             <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-            <meta property="og:image" content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg" />
+            <meta property="og:image" content="https://www.salnet.xyz/download.jpeg" />
             <title>Sal - Sahil</title>
         </>
     )
