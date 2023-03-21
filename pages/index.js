65832b3716dd818e01c0080efb924f5075e09bf6
sahilnetic <jsthil99@gmail.com>
2023-03-21 09:15:46 +0530

    doni

--- pages/index.js
+++ pages/index.js
@@ -60,9 +60,8 @@ const Home = ()=>{
                                 </svg>
                                 <Link href={"https://twitter.com/salnetxyz"}>twitter</Link></li>
                                 <li className="flex">
-                                <svg className="translate-y-2 -translate-x-1.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-                                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-                                </svg> <Link href={"https://github.com/salnetxyz"}>github</Link></li>
+                                <svg className="translate-y-2 -translate-x-1.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>
+                                <Link href={"https://www.youtube.com/@salnetxyz"}>youtube</Link></li>
                               </ul>
                             </div>
                             </div>
@@ -98,17 +97,7 @@ const Home = ()=>{
                                 </header>
                             </article>  
     
-                            <article>
-                                <header>
-                                    <h2 className="text-lg text-neutral-800 flex">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 -translate-x-1 translate-y-3">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-                                    </svg>
-                                      <Link href={"https://github.com/salnetxyz/portfolio/fork"}>fork on github</Link>
-                                    </h2>
-                                    <p className="text-xs text-neutral-600">github.com/salnetxyz/portfolio/fork</p>
-                                </header>
-                            </article>
+
 
                             </section>
 
